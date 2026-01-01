efeca26a28adb5bf5d2262fcb11f291726df29aa
Bordás Dániel <bordas.daniel0124@gmail.com>
2026-01-01 18:26:40 +0100

    Továbbiak kártya hotfix

--- konyvkocka/src/components/common/Card.tsx
+++ konyvkocka/src/components/common/Card.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import showMoreCardImage from '../../assets/img/carousel.jpg';
 
 export interface CardData {
   id: string;
@@ -218,7 +219,7 @@ const Card: React.FC<CardProps> = ({
         {showMoreCard && (
           <div className={gridClass}>
             <div className="card">
-              <img src="/assets/img/carousel.jpg" className="card-img-top card-img-blur" alt="További tartalmak" />
+              <img src={showMoreCardImage} className="card-img-top card-img-blur" alt="További tartalmak" />
               <div className="card-body">
                 <h5 className="card-title">Továbbiak</h5>
                 <p className="card-text">Fedezd fel további ajánlatainkat és kategóriáinkat a teljes kínálatért.</p>
