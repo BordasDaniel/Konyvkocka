7325d159288580fbd13b2a1ee8efae3a5ed7d603
Dudás Ádám <dudasa949@gmail.com>
2026-02-01 23:58:00 +0100

    Subscription végpont kibővítve. Előzmények végpont kiépítése, szükséges DTO-k létrehozva.

---
+++ KonyvkockaAPI/DTO/Response/SubscriptionInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace KonyvkockaAPI.DTO.Response
+{
+    public class SubscriptionInfoDTO
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool AutoRenew { get; set; }
+        public string Price { get; set; }
+        public string Message { get; set; }
+    }
+
+}
