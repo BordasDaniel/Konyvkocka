257bcb6e7a0e0a28e5c495c8f1e997cdf41fd9ea
Dudas Adam <DudasA@WINDOWS-QRH6DLJ-Andrassy>
2026-02-19 11:34:00 +0100

    Új ranglista végpontok és DTOk.

--- KonyvkockaAPI/DTO/Response/LeaderboardEntryDTO.cs
+++ KonyvkockaAPI/DTO/Response/LeaderboardEntryDTO.cs
@@ -3,18 +3,16 @@
     public class LeaderboardEntryDTO
     {
         public int Rank { get; set; }
-        public string Username { get; set; }
-        public string Avatar { get; set; }
-        public string CountryFlag { get; set; }
-        public string CountryCode { get; set; }
-        public string Continent { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Avatar { get; set; } = null!;
+        public string CountryCode { get; set; } = null!;
+        public bool IsPremium { get; set; }
         public int Points { get; set; }
-        public int BooksRead { get; set; }
-        public int MediaWatched { get; set; }
-        public int CompletionRate { get; set; }
-        public int LongestStreak { get; set; }
+        public int BookCount { get; set; }
+        public int MediaCount { get; set; }
+        public double CompletionPct { get; set; }
         public int Level { get; set; }
-        public bool IsSubscriber { get; set; }
-        public DateTime JoinDate { get; set; }
+        public int DayStreak { get; set; }
     }
 }
